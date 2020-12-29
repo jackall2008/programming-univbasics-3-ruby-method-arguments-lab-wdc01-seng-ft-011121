0240b087d9991bc31c98b228095095e2401c6930
julian.patterson <julian.patterson@gmail.com>
2020-12-29 18:01:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,9 +18,8 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-introduction_with_language_optional("JuJu", language)
+introduction_with_language_optional("JuJu")
 
-introduction_with_language_optional
 
 
 
